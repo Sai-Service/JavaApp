e3a1ac0517e0728703ad4a482dfb43d9b378f0f5
IT-HARSH-9594 <edpborivali@saiservice.com>
2025-08-22 23:59:01 +0530

    22-08-2025 - harsh - api for true value vehicle details for wash - goa

--- src/main/java/com/sai/erp/master/dao/SsVehWashingRegisterGaDao.java
+++ src/main/java/com/sai/erp/master/dao/SsVehWashingRegisterGaDao.java
@@ -59,4 +59,57 @@ public interface SsVehWashingRegisterGaDao extends CrudRepository<SsVehWashingRe
             + " NVL(STATUS,'-') STATUS  FROM SS_VEH_WASHING_REGISTER_GA \n"
             + "WHERE CHASSIS_NO=?1 and OU_ID=?2 ORDER BY CREATION_DATE DESC FETCH FIRST 1 ROWS ONLY", nativeQuery = true)
     public List<Map> getVehWashOutSalesGaDetailsByChassisNoAndOuId(String chassisNo, Integer ouId);
+
+    //used to get details for new veh in master table ALONG WITH vin no.
+    @Query(value = "  SELECT distinct  GFT.VIN, cii.instance_number, msib.attribute6 chassis_no,\n"
+            + "                   msib.attribute12 engine_no, cii.install_date registration_date,\n"
+            + "                   hca.account_number,\n"
+            + "                   CASE\n"
+            + "                      WHEN hp.party_type = 'PERSON'\n"
+            + "                         THEN    hp.person_first_name\n"
+            + "                              || ' '\n"
+            + "                              || hp.person_middle_name\n"
+            + "                              || ' '\n"
+            + "                              || hp.person_last_name\n"
+            + "                      ELSE party_name\n"
+            + "                   END cust_name,\n"
+            + "                   msib.inventory_item_id,\n"
+            + "                   (SELECT MAX (variant_cd)\n"
+            + "                      FROM gd_fdi_trans\n"
+            + "                     WHERE ge1 = cii.instance_number AND trans_type = 'WI') variant_code,\n"
+            + "                   (SELECT fvl.meaning\n"
+            + "                      FROM (SELECT   gd.variant_cd\n"
+            + "                                FROM gd_fdi_trans gd\n"
+            + "                               WHERE gd.ge1 = cii.instance_number AND gd.trans_type = 'WI'\n"
+            + "                            ORDER BY gd.trans_date DESC) a,\n"
+            + "                           fnd_lookup_values_vl fvl\n"
+            + "                     WHERE ROWNUM = 1\n"
+            + "                       AND fvl.lookup_code = variant_cd\n"
+            + "                       AND attribute_category = 'SS_MSIL_SALES_CODE') vehicle_desc,\n"
+            + "                   (SELECT *\n"
+            + "                      FROM (SELECT   executive\n"
+            + "                                FROM gd_fdi_trans\n"
+            + "                               WHERE ge1 = cii.instance_number AND trans_type = 'WI'\n"
+            + "                            ORDER BY trans_date DESC) a\n"
+            + "                     WHERE ROWNUM = 1) service_advisor\n"
+            + "              FROM csi_item_instances cii,\n"
+            + "              gd_fdi_trans gft,\n"
+            + "                   mtl_system_items_b msib,\n"
+            + "                   hz_parties hp,\n"
+            + "                   hz_cust_accounts hca\n"
+            + "            WHERE cii.instance_number = ?1\n"
+            + "               and cii.inventory_item_id = msib.inventory_item_id\n"
+            + "               and cii.instance_number = gft.ge1\n"
+            + "               and hp.party_id = cii.owner_party_id\n"
+            + "               and cii.owner_party_account_id = hca.cust_account_id\n"
+            + "               and msib.organization_id = 119", nativeQuery = true)
+    public List<Map> getVehDetailsCsiByRegNo(String regNo);
+
+    
+    //query to fetch true value veh details for washing in
+    @Query(value = "SELECT nvl(REG_NO,'-') REG_NO, NVL(CHASSIS_NO,'-') CHASSIS_NO, NVL(VIN,'-') VIN,\n"
+            + "NVL(ENGINE_NO,'-') ENGINE_NO, NVL(MODEL_DESC,'-') MODEL_DESC, NVL(VARIANT_DESC,'-') VARIANT_DESC,\n"
+            + "NVL(VARIANT_CODE,'-') VARIANT_CODE, NVL(FUEL_DESC,'-') FUEL_DESC, NVL(MANUFACTURER,'-') MANUFACTURER,\n"
+            + "NVL(DSE_NAME,'-') DSE_NAME FROM SS_DMS_STOCK_TV WHERE REG_NO=:regNo", nativeQuery = true)
+    public List<Map> getTvVehDetailsByRegNo(String regNo);
 }
